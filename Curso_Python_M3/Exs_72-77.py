5f4426961d8708fe494eb46e94da26f0be929c69
Matheus Rauen <matheus.rauen@grad.ufsc.br>
2022-08-22 18:42:12 -0300

    Tuples module uploaded

--- Curso_Python_M3/Exs_72-77.py
+++ Curso_Python_M3/Exs_72-77.py
@@ -1,120 +1,120 @@
 #Ex72
-# tup = (
-# 'zero',
-# 'um',
-# 'dois',
-# 'três',
-# 'quatro',
-# 'cinco',
-# 'seis',
-# 'sete',
-# 'oito',
-# 'nove',
-# ' dez',
-# ' onze',
-# ' doze',
-# ' treze',
-# ' quatorze ou catorze',
-# 'quinze',
-# ' dezesseis',
-# ' dezessete',
-# ' dezoito',
-# ' dezenove',
-# ' vinte'
-# )
+tup = (
+'zero',
+'um',
+'dois',
+'três',
+'quatro',
+'cinco',
+'seis',
+'sete',
+'oito',
+'nove',
+' dez',
+' onze',
+' doze',
+' treze',
+' quatorze ou catorze',
+'quinze',
+' dezesseis',
+' dezessete',
+' dezoito',
+' dezenove',
+' vinte'
+)
 
-# numero = int(input('Digite um número (0-20): '))
-# print('Você digitou o número {}'.format(tup[numero]))
+numero = int(input('Digite um número (0-20): '))
+print('Você digitou o número {}'.format(tup[numero]))
 
 #Ex73
-# tabela_times = (
-# 'Palmeiras',
-# 'Fluminense',
-# 'Flamengo',
-# 'Corinthians',
-# 'Athletico',
-# 'Internacio',
-# 'Atlético',
-# 'América',
-# 'Red Bull Bragantino',
-# 'Santos',
-# 'Goiás',
-# 'São Paulo',
-# 'Botafogo',
-# 'Ceará',
-# 'Fortaleza',
-# 'Cuiabá',
-# 'Avaí',
-# 'Atlético',
-# 'Juventude'
-# )
+tabela_times = (
+'Palmeiras',
+'Fluminense',
+'Flamengo',
+'Corinthians',
+'Athletico',
+'Internacio',
+'Atlético',
+'América',
+'Red Bull Bragantino',
+'Santos',
+'Goiás',
+'São Paulo',
+'Botafogo',
+'Ceará',
+'Fortaleza',
+'Cuiabá',
+'Avaí',
+'Atlético',
+'Juventude'
+)
 
-# print('#'*20)
-# print('RESULTADO')
-# print('#'*20)
+print('#'*20)
+print('RESULTADO')
+print('#'*20)
 
-# print('Os 5 primeiros times são: {}\n'.format(tabela_times[0:5]))
-# print('Os últimos 4 colocados são: {}\n'.format(tabela_times[-4:]))
-# print('Os times em ordem alfabética são: {}\n'.format(sorted(tabela_times)))
-# print('O Avaí (SC) está na posição: {}\n'.format(tabela_times.index('Avaí')))
+print('Os 5 primeiros times são: {}\n'.format(tabela_times[0:5]))
+print('Os últimos 4 colocados são: {}\n'.format(tabela_times[-4:]))
+print('Os times em ordem alfabética são: {}\n'.format(sorted(tabela_times)))
+print('O Avaí (SC) está na posição: {}\n'.format(tabela_times.index('Avaí')))
 
 #Ex74 
-# from random import randint 
+from random import randint 
 
-# randtup = (randint(1,15), randint(1,15), randint(1,15), randint(1,15), randint(1,15))
-# maior_val = 0
-# menor_valor = 15
+randtup = (randint(1,15), randint(1,15), randint(1,15), randint(1,15), randint(1,15))
+maior_val = 0
+menor_valor = 15
 
-# for maior in randtup:
-#     if maior > maior_val:
-#         maior_val = maior
+for maior in randtup:
+    if maior > maior_val:
+        maior_val = maior
     
-# for menor in randtup:
-#     if menor < menor_valor:
-#         menor_valor = menor
+for menor in randtup:
+    if menor < menor_valor:
+        menor_valor = menor
 
-# print('Valores gerados: {}'.format(randtup))
-# print('Maior valor dos gerados: {}'.format(maior_val))
-# print('Menor valor dos gerados: {}'.format(menor_valor))
+print('Valores gerados: {}'.format(randtup))
+print('Maior valor dos gerados: {}'.format(maior_val))
+print('Menor valor dos gerados: {}'.format(menor_valor))
 
 #Ex75
-# valores_lidos = (
-#     int(input('Primeiro valor: ')), int(input('Segundo valor: ')), int(input('Terceiro valor: ')), int(input('Quarto valor: '))
-#     )
+valores_lidos = (
+    int(input('Primeiro valor: ')), int(input('Segundo valor: ')), int(input('Terceiro valor: ')), int(input('Quarto valor: '))
+    )
         
-# print('Vezes que apareceram o número 9: {}\n'.format(valores_lidos.count(9)))
-# print('Primeira posição digitado o número 3: {}\n'.format((valores_lidos.index(3)+1)))
-# print('Números pares: ', end='')
+print('Vezes que apareceram o número 9: {}\n'.format(valores_lidos.count(9)))
+print('Primeira posição digitado o número 3: {}\n'.format((valores_lidos.index(3)+1)))
+print('Números pares: ', end='')
 
-# for valor in valores_lidos:
-#     if (valor % 2 == 0):
-#         print(valor, end=' ')
+for valor in valores_lidos:
+    if (valor % 2 == 0):
+        print(valor, end=' ')
 
 #Ex76 
-# item_e_precos = (
-#     'Lápis',
-#     1.50,
-#     'Borracha',
-#     2.50, 
-#     'Caneta',
-#     8.90,
-#     'Caderno',
-#     22.90
-# )
+item_e_precos = (
+    'Lápis',
+    1.50,
+    'Borracha',
+    2.50, 
+    'Caneta',
+    8.90,
+    'Caderno',
+    22.90
+)
 
-# header = """
-# \n
-# ============================
-# LISTAGEM DE PREÇOS FINAL: 
-# ============================
-# \n
-# """
-# print(header.center(50))
-# for valor in range(0, len(item_e_precos)):
-#         if (valor % 2 == 0):
-#             print('{:.<30}'.format(item_e_precos[valor]), end='')
-#         else:
-#             print('R$ {:>3.2f}'.format(item_e_precos[valor]))
+header = """
+\n
+============================
+LISTAGEM DE PREÇOS FINAL: 
+============================
+\n
+"""
+print(header.center(50))
+for valor in range(0, len(item_e_precos)):
+        if (valor % 2 == 0):
+            print('{:.<30}'.format(item_e_precos[valor]), end='')
+        else:
+            print('R$ {:>3.2f}'.format(item_e_precos[valor]))
 
 #Ex77 
 palavras = (
@@ -125,9 +125,11 @@ palavras = (
     'python'
 )
 
-msg = "Na palavra '{}' temos as vogais: {}"
-
 for palavra in palavras:
-    for letras in palavras:
-        if letras == 'aeiou':
-            print(msg.format(palavra, letras), end=' ')
+    print("\nNa palavra '{}' temos: ".format(palavra), end='')
+    for letra in palavra: 
+        if letra.lower() in ('a', 'e', 'i', 'o', 'u'):
+            print(letra, end=' ')
+            
+        
+            
